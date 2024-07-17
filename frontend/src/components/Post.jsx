0b16b354aa6915b6bf63db16713178a4696903da
Iterate-AI <support@iterate.ai.com>
2024-07-17 12:55:01 +0530

    Apply diff to multiple files

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -126,12 +126,7 @@ export function Post({ post, isExpanded = false, postIndex, setCommentMode }) {
                 style={{ position: "absolute", top: 0, left: 0 }}
               />
             ) : (
-              <img
-                onClick={() => {
-                  mp.track("Post Media Clicked", { time: new Date() });
-
-                  onMediaClick("image");
-                }}
+              <img onClick={() => { mixpanel.track('wqf', { 'qfq': 'qwfwqfw' }); onMediaClick("image"); }}
                 loading="lazy"
                 width="auto"
                 height="100%"
